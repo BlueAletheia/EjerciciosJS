34e9f0a259e963d38f2c31d00650e4bcfdf1fa35
aliciafdezsoy <108926165+aliciafdezsoy@users.noreply.github.com>
2022-07-21 15:01:54 +0200

    Ejercicio1 uploaded

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const arrNames = [
+    {id: 1, name: 'Pepe'},
+    {id: 2, name: 'Juan'},
+    {id: 3, name: 'Alba'},
+    {id: 4, name: 'Toby'},
+    {id: 5, name: 'Lala'}
+  ]
+  
+  let name = arrNames.find(function(name) {
+    return name.id === 3;
+  });
+  
+  console.log(name);
+  
