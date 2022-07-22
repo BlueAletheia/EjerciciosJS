b2cb225ae53fc4f13d2e7cfceeedfa7665494378
aliciafdezsoy <108926165+aliciafdezsoy@users.noreply.github.com>
2022-07-22 11:40:11 +0200

    Ejercicio2 uploaded

---
+++ Ejercicio2.js
@@ -0,0 +1,20 @@
+const arrDirty = [NaN, 0, 5, false, -1, "", undefined, 3, null, "test"];
+
+const check = (arr) => {
+  console.log("---")
+  return arr.map((item) => {
+    if (
+      item !== false &&
+      item !== null &&
+      item !== undefined &&
+      !isNaN(item) &&
+      item !== ""
+    ) {
+      return item;
+    } else {
+      return console.log("no valido");
+    }
+  });
+};
+
+check(arrDirty);
--- ejercicio1.js
+++ ejercicio1.js
@@ -11,4 +11,4 @@ const arrNames = [
   });
   
   console.log(name);
-  
+ 
