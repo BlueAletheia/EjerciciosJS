eef7362b8def93a8d3108f46b0910bc5ea63a0b5
aliciafdezsoy <108926165+aliciafdezsoy@users.noreply.github.com>
2022-07-25 12:27:12 +0200

    Uploaded Ejercicio11

--- Ejercicio10.js
+++ Ejercicio10.js
@@ -1,8 +1,12 @@
-
-const removeHTMLTags = (text) => {
-    return text.replace(/<[^>]*>?/g, '');
-}
-const result = removeHTMLTags('<div><span>lorem</span> <strong>ipsum</strong></div>');
- 
-console.log(result); // lorem ipsum
-
+let splitArray = (array, chunks) => {
+    let result = [];
+    for(let i = 0; i < array.length; i += chunks){
+      result.push(array.slice(i, i+chunks));
+    }
+    return result;
+  }
+  
+  
+  const result = splitArray([1, 2, 3, 4, 5, 6, 7], 3);
+  console.log(result); // [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7 ] ]
+  
