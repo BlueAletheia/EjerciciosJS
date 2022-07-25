981e13c1344236cdce3f66ffb143cac807d7a13d
aliciafdezsoy <108926165+aliciafdezsoy@users.noreply.github.com>
2022-07-25 09:19:39 +0200

    updated ejercicio6

--- Ejercicio6.js
+++ Ejercicio6.js
@@ -1,10 +1,11 @@
-//const roundedResult = roundTo(2.123, 2);
- 
-const roundedResult = roundTo(1.123456789, 6);
+const roundedResult1 = roundTo(2.123, 2);
 
-function roundTo(a, b){
-    let exponential = 10**b;
-    return Math.round(a*exponential)/exponential;
-  }
-  
-  console.log(roundedResult);
+const roundedResult2 = roundTo(1.123456789, 6);
+
+function roundTo(a, b) {
+    let exponential = 10 ** b;
+    return Math.round(a * exponential) / exponential;
+}
+
+console.log(roundedResult1);
+console.log(roundedResult2);
